c5df7fc18f81db2d94e0175c4930024e3f09df95
Evan Wright <ecwright595@my.pittcc.edu>
2020-12-06 02:59:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Site
+
+Currently in progress as I continue to add sections. 
---
+++ js/main.js
@@ -0,0 +1,5 @@
+particlesJS.load("particles-js", "../assets/json/particles.json", () => {
+  console.log("callback - particles.js config loaded");
+});
+
+console.log("test");
